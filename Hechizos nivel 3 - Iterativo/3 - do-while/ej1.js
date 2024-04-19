686ea3c9c1ba01a5258d27d20a2efe00b04bb600
pc 2 <>
2024-04-19 11:31:24 -0300

    iniciando el do while

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -1,8 +1,54 @@
 const leer = require("prompt-sync")();
 
+const DAÑO_HORROCRUXES_CORDURA=5.7
+const DAÑO_HORROCRUXES_SALUD=10.4
+const MIN_CODIGO1=-3
+const MAX_CODIGO2=20
 function main() {
+let horrocruxesDestruidos=0;
+let vidaPersonaje=400;
+let corduraPersonaje=200;
+let turnos=0;
+let codigoIngresado="def codigo ingresado";
+let codigo1="R1dd13";
+let codigo2="G4unt!";
+let codigo3="H3l?ga!";
+let codigo4="?R4vena";
+let codigo5="N@9ini?";
+let minCodigo1=MIN_CODIGO1
+let maxCodigo1=MAX_CODIGO2
+let posibilidadProteccion=50
+let probabilidadEsquivo=0;
+let eleccion_esquivar=0
+
+do {
+    x=Math.floor(Math.random() * (maxCodigo1 - minCodigo1) + minCodigo1);
+    console.log(x);
+    console.log("Escribe correctamente el codigo secreto");
     
+    codigoIngresado=leer()
+    
+    if (codigoIngresado=codigo1+x) {
+        console.log("Has adivinado el codigo secreto");
+        horrocruxesDestruidos=horrocruxesDestruidos+1
+        posibilidadProteccion=posibilidadProteccion-11.875
+
+        
+    }else{
+        console.log("Has fallado el codigo secreto");
+        probabilidadEsquivo=Math.floor(Math.random() *101)
+        if (probabilidadEsquivo<posibilidadProteccion) {
+            console.log("Gracias a tu agilidad has podido evitar el ataque ingrese 1 para esquivar el daño de cordura y 2 para esquivar el daño de salud");
+            eleccion_esquivar=leer();
+        }
+    }   
+    
+} while (vidaPersonaje>0 && corduraPersonaje>0 && turnos<30);
+
+
+
 }
+
 
 
 main();
