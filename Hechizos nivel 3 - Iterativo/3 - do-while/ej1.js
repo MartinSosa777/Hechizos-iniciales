a2bb634fa3d0c1b52aefeddae43d456a0da4c103
Tito <sosamartin567@gmail.com>
2024-05-09 10:04:47 -0300

    copia de seguridad

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -2,7 +2,7 @@ const leer = require("prompt-sync")();
 const DAÑO_HORROCRUXES_CORDURA = 5.7;
 const DAÑO_HORROCRUXES_SALUD = 10.4;
 const PROBABILIDAD_MUERTE_INSTANTANEA = 5;
-const AUMENTO_PROBABILIDAD_HORROCRUX_DESTRUIDO = 2.4;
+const AUMENTO_PROBABILIDAD_HORROCRUX_DESTRUIDO= 2.4;
 
 let horrocruxesDestruidos = 0;
 let vidaPersonaje = 400;
@@ -40,8 +40,152 @@ function codigoSecretoNagini(min, max) {
 }
 
 function main() {
-
+  do {
+    /*if (Math.random() * 100 < PROBABILIDAD_MUERTE_INSTANTANEA) {
+      vidaPersonaje = 0;
+      corduraPersonaje = 0;
+      console.log("El personaje ha perdido toda su salud y cordura");*/
+    turnos++;
+    console.log(
+      "Los horrocruxes no pueden derrotarte de inmediato debido a que es tu primer turno"
+    );
+    while (
+      horrocruxesDestruidos === 0 &&
+      vidaPersonaje > 0 &&
+      corduraPersonaje > 0
+    ) {
+      console.log(
+        "Ingrese el numero aleatorio donde se encontraria el codigo R1dd13?"
+      );
+      codigoIngresado = Number(leer());
+      secretoResuelto1 = 1; //secretoResuelto1 = codigoSecretoRiddle(-3, 21);
+      console.log(secretoResuelto1);
+      if (codigoIngresado === secretoResuelto1) {
+        console.log(
+          `Adivinaste el numero, destruiste el Horrocrux R1ddl3${secretoResuelto1} `
+        );
+        horrocruxesDestruidos++;
+        turnos++;
+      } else {
+        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+        console.log(
+          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
+        );
+        turnos++;
+      }
+    }
+    while (
+      horrocruxesDestruidos === 1 &&
+      vidaPersonaje > 0 &&
+      corduraPersonaje > 0
+    ) {
+      console.log(
+        "Ingrese el numero aleatorio donde se encontraria el codigo ?G4unt!"
+      );
+      codigoIngresado = leer();
+      secretoResuelto2 = 2; //secretoResuelto2 = codigoSecretoGaunt(-100, -71);
+      console.log(secretoResuelto2);
+      if (codigoIngresado == secretoResuelto2) {
+        console.log(
+          `Adivinaste el numero, destruiste el Horrocrux ?G4aunt!${secretoResuelto2} `
+        );
+        horrocruxesDestruidos++;
+        turnos++;
+      } else {
+        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+        console.log(
+          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
+        );
+        turnos++;
+      }
+    }
+    while (
+      horrocruxesDestruidos === 2 &&
+      vidaPersonaje > 0 &&
+      corduraPersonaje > 0
+    ) {
+      console.log(
+        "Ingrese el número aleatorio donde se encontraría el código de H3l?ga!:"
+      );
+      codigoIngresado = leer();
+      secretoResuelto3 = 3; //secretoResuelto3 = codigoSecretoHelga(4, 13);
+      console.log(secretoResuelto3);
+      if (codigoIngresado == secretoResuelto3) {
+        console.log(
+          `Adivinaste el numero, destruiste el Horrocrux H3l?ga!${secretoResuelto3} `
+        );
+        horrocruxesDestruidos++;
+        turnos++;
+      } else {
+        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+        console.log(
+          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
+        );
+        turnos++;
+      }
+    }
+    while (
+      horrocruxesDestruidos === 3 &&
+      vidaPersonaje > 0 &&
+      corduraPersonaje > 0
+    ) {
+      console.log(
+        "Ingrese el caracter aleatorio donde se encontraría el código de ?R4vena:"
+      );
+      codigoIngresado = leer();
+      secretoResuelto4 = codigoSecretoRavenclaw(97, 98);
+      console.log(secretoResuelto4.toLowerCase());
+      if (codigoIngresado == secretoResuelto4) {
+        console.log(
+          `Adivinaste el caracter, destruiste el Horrocrux ?R4vena:${secretoResuelto4}`
+        );
+        horrocruxesDestruidos++;
+        turnos++;
+      } else {
+        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+        console.log(
+          `Caracter no valido, el Horrocrux te infligio daño.Tu vida actual es:${vidaPersonaje} y tu cordura es:${corduraPersonaje}`
+        );
+        turnos++;
+      }
+      while (
+        horrocruxesDestruidos === 4 &&
+        vidaPersonaje > 0 &&
+        corduraPersonaje > 0
+      )
+      {
+        console.log(
+          `Ingrese el caracter aleatorio donde se encontraría el código de N@9ini?:${secretoResuelto5}`
+        );
+        codigoIngresado = leer();
+        secretoResuelto5 = codigoSecretoNagini(72, 73);
+        console.log(secretoResuelto5.toUpperCase());
+    if (codigoIngresado == secretoResuelto5) {
+      console.log(
+        `Adivinaste el caracter, destruiste el Horrocrux N@9ini?${secretoResuelto5}`
+      );
+      horrocruxesDestruidos++;
+      turnos++;
+    } else {
+      vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+      corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+      console.log(
+        `Caracter no valido, el Horrocrux te infligio daño.Tu vida actual es:${vidaPersonaje} y tu cordura es:${corduraPersonaje}`
+      );
+      turnos++;
+    }}
 }
+   } while (turnos<=30 && horrocruxesDestruidos < 5 && corduraPersonaje>0 && vidaPersonaje>0)
+    if (horrocruxesDestruidos === 5) {
+      FINAL_BUENO();
+    }else if(vidaPersonaje<=0 || corduraPersonaje<=0 || turnos==30 ){
+      FINAL_MALO();
+    }
+  }
 
 main();
 function FINAL_BUENO() {
@@ -68,3 +212,4 @@ function FINAL_MALO() {
     "Pero incluso en la oscuridad más profunda, la llama de la esperanza aún arde, recordando que la lucha nunca termina y que el mañana siempre guarda la promesa de una nueva oportunidad para la redención y la victoria."
   );
 }
+
