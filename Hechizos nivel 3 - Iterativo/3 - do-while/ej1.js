1df31744d1b931d6c947ad645b335213b2b66788
pc 2 <>
2024-05-16 11:15:52 -0300

    modularizado mejorado

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -2,7 +2,7 @@ const leer = require("prompt-sync")();
 const DAÑO_HORROCRUXES_CORDURA = 5.7;
 const DAÑO_HORROCRUXES_SALUD = 10.4;
 const PROBABILIDAD_MUERTE_INSTANTANEA = 5;
-const AUMENTO_PROBABILIDAD_HORROCRUX_DESTRUIDO= 2.4;
+const AUMENTO_PROBABILIDAD_HORROCRUX_DESTRUIDO = 2.4;
 
 let horrocruxesDestruidos = 0;
 let vidaPersonaje = 400;
@@ -22,6 +22,86 @@ let secretoResuelto2 = 0;
 let secretoResuelto3 = 0;
 let secretoResuelto4 = 0;
 let secretoResuelto5 = 0;
+
+
+function secretoResueltoNa9ini() {
+  console.log(
+    `Ingrese el caracter aleatorio donde se encontraría el código de N@9ini?:${secretoResuelto5}`
+  );
+  codigoIngresado = leer();
+  secretoResuelto5 = codigoSecretoNagini(72, 73);
+  console.log(secretoResuelto5.toUpperCase());
+}
+
+function secretoResueltoR4vena() {
+  console.log(
+    "Ingrese el caracter aleatorio donde se encontraría el código de ?R4vena:"
+  );
+  codigoIngresado = leer();
+  secretoResuelto4 = codigoSecretoRavenclaw(97, 98);
+  console.log(secretoResuelto4.toLowerCase());
+}
+
+function secretoResueltoH3lga() {
+  console.log(
+    "Ingrese el número aleatorio donde se encontraría el código de H3l?ga!:"
+  );
+  codigoIngresado = leer();
+  secretoResuelto3 = codigoSecretoHelga(4, 13);
+  console.log(secretoResuelto3);
+}
+
+function secretoResueltoG4aunt() {
+  console.log(
+    "Ingrese el numero aleatorio donde se encontraria el codigo ?G4unt!"
+  );
+  codigoIngresado = leer();
+  secretoResuelto2 = codigoSecretoGaunt(-100, -71);
+  console.log(secretoResuelto2);
+}
+
+function dañoTotal() {
+  vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
+  corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
+  console.log(
+    `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
+  );
+  turnos++;
+}
+
+function secretoResueltoR1ddl3() {
+  console.log(
+    "Ingrese el numero aleatorio donde se encontraria el codigo R1dd13?"
+  );
+  codigoIngresado = Number(leer());
+  secretoResuelto1 = codigoSecretoRiddle(-3, 21);
+  console.log(secretoResuelto1);
+}
+
+function FINAL_BUENO() {
+  console.log(
+    "¡Victoria para el estudiante valiente que, con coraje y determinación, ha destruido todos los Horrocruxes!"
+  );
+  console.log(
+    "Con cada fragmento de alma oscura eliminado, la luz de la esperanza ha brillado más brillante sobre el mundo mágico."
+  );
+  console.log(
+    "¡Su sacrificio y valentía han salvado a nuestra comunidad de las sombras de la oscuridad, asegurando un futuro lleno de paz y prosperidad para todas las generaciones venideras!"
+  );
+}
+
+function FINAL_MALO() {
+  console.log("En el oscuro manto de la derrota...");
+  console.log(
+    "El estudiante enfrentó una verdad devastadora: a pesar de sus esfuerzos incansables, los Horrocruxes permanecen intactos, y la sombra del mal se alza triunfante sobre el mundo mágico."
+  );
+  console.log(
+    "Aunque la batalla fue ardua y valiente, el destino ha dictado su veredicto, dejando al estudiante con el amargo sabor de la derrota."
+  );
+  console.log(
+    "Pero incluso en la oscuridad más profunda, la llama de la esperanza aún arde, recordando que la lucha nunca termina y que el mañana siempre guarda la promesa de una nueva oportunidad para la redención y la victoria."
+  );
+}
 
 function codigoSecretoRiddle(min, max) {
   return Math.random() * (max - min) + min;
@@ -54,12 +134,7 @@ function main() {
       vidaPersonaje > 0 &&
       corduraPersonaje > 0
     ) {
-      console.log(
-        "Ingrese el numero aleatorio donde se encontraria el codigo R1dd13?"
-      );
-      codigoIngresado = Number(leer());
-      secretoResuelto1 = 1; //secretoResuelto1 = codigoSecretoRiddle(-3, 21);
-      console.log(secretoResuelto1);
+      secretoResueltoR1ddl3();
       if (codigoIngresado === secretoResuelto1) {
         console.log(
           `Adivinaste el numero, destruiste el Horrocrux R1ddl3${secretoResuelto1} `
@@ -67,12 +142,7 @@ function main() {
         horrocruxesDestruidos++;
         turnos++;
       } else {
-        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
-        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
-        console.log(
-          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
-        );
-        turnos++;
+        dañoTotal();
       }
     }
     while (
@@ -80,12 +150,7 @@ function main() {
       vidaPersonaje > 0 &&
       corduraPersonaje > 0
     ) {
-      console.log(
-        "Ingrese el numero aleatorio donde se encontraria el codigo ?G4unt!"
-      );
-      codigoIngresado = leer();
-      secretoResuelto2 = 2; //secretoResuelto2 = codigoSecretoGaunt(-100, -71);
-      console.log(secretoResuelto2);
+      secretoResueltoG4aunt();
       if (codigoIngresado == secretoResuelto2) {
         console.log(
           `Adivinaste el numero, destruiste el Horrocrux ?G4aunt!${secretoResuelto2} `
@@ -93,12 +158,7 @@ function main() {
         horrocruxesDestruidos++;
         turnos++;
       } else {
-        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
-        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
-        console.log(
-          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
-        );
-        turnos++;
+        dañoTotal();
       }
     }
     while (
@@ -106,12 +166,7 @@ function main() {
       vidaPersonaje > 0 &&
       corduraPersonaje > 0
     ) {
-      console.log(
-        "Ingrese el número aleatorio donde se encontraría el código de H3l?ga!:"
-      );
-      codigoIngresado = leer();
-      secretoResuelto3 = 3; //secretoResuelto3 = codigoSecretoHelga(4, 13);
-      console.log(secretoResuelto3);
+      secretoResueltoH3lga();
       if (codigoIngresado == secretoResuelto3) {
         console.log(
           `Adivinaste el numero, destruiste el Horrocrux H3l?ga!${secretoResuelto3} `
@@ -119,12 +174,7 @@ function main() {
         horrocruxesDestruidos++;
         turnos++;
       } else {
-        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
-        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
-        console.log(
-          `Numero no valido, el Horrocrux te infligio daño. Tu vida actual es:${vidaPersonaje}, y tu cordura:${corduraPersonaje}`
-        );
-        turnos++;
+        dañoTotal();
       }
     }
     while (
@@ -132,12 +182,7 @@ function main() {
       vidaPersonaje > 0 &&
       corduraPersonaje > 0
     ) {
-      console.log(
-        "Ingrese el caracter aleatorio donde se encontraría el código de ?R4vena:"
-      );
-      codigoIngresado = leer();
-      secretoResuelto4 = codigoSecretoRavenclaw(97, 98);
-      console.log(secretoResuelto4.toLowerCase());
+      secretoResueltoR4vena();
       if (codigoIngresado == secretoResuelto4) {
         console.log(
           `Adivinaste el caracter, destruiste el Horrocrux ?R4vena:${secretoResuelto4}`
@@ -145,71 +190,32 @@ function main() {
         horrocruxesDestruidos++;
         turnos++;
       } else {
-        vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
-        corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
-        console.log(
-          `Caracter no valido, el Horrocrux te infligio daño.Tu vida actual es:${vidaPersonaje} y tu cordura es:${corduraPersonaje}`
-        );
-        turnos++;
+        dañoTotal();
       }
       while (
         horrocruxesDestruidos === 4 &&
         vidaPersonaje > 0 &&
         corduraPersonaje > 0
-      )
-      {
-        console.log(
-          `Ingrese el caracter aleatorio donde se encontraría el código de N@9ini?:${secretoResuelto5}`
-        );
-        codigoIngresado = leer();
-        secretoResuelto5 = codigoSecretoNagini(72, 73);
-        console.log(secretoResuelto5.toUpperCase());
-    if (codigoIngresado == secretoResuelto5) {
-      console.log(
-        `Adivinaste el caracter, destruiste el Horrocrux N@9ini?${secretoResuelto5}`
-      );
-      horrocruxesDestruidos++;
-      turnos++;
-    } else {
-      vidaPersonaje -= DAÑO_HORROCRUXES_SALUD;
-      corduraPersonaje -= DAÑO_HORROCRUXES_CORDURA;
-      console.log(
-        `Caracter no valido, el Horrocrux te infligio daño.Tu vida actual es:${vidaPersonaje} y tu cordura es:${corduraPersonaje}`
-      );
-      turnos++;
-    }}
-}
-   } while (turnos<=30 && horrocruxesDestruidos < 5 && corduraPersonaje>0 && vidaPersonaje>0)
-    if (horrocruxesDestruidos === 5) {
-      FINAL_BUENO();
-    }else if(vidaPersonaje<=0 || corduraPersonaje<=0 || turnos==30 ){
-      FINAL_MALO();
+      ) {
+        secretoResueltoNa9ini();
+        if (codigoIngresado == secretoResuelto5) {
+          console.log(
+            `Adivinaste el caracter, destruiste el Horrocrux N@9ini?${secretoResuelto5}`
+          );
+          horrocruxesDestruidos++;
+          turnos++;
+        } else {
+          dañoTotal();
+        }
+      }
     }
+  } while (turnos <= 30 && horrocruxesDestruidos < 5 && corduraPersonaje > 0 && vidaPersonaje > 0)
+  if (horrocruxesDestruidos === 5) {
+    FINAL_BUENO();
+  } else if (vidaPersonaje <= 0 || corduraPersonaje <= 0 || turnos == 30) {
+    FINAL_MALO();
   }
+}
 
 main();
-function FINAL_BUENO() {
-  console.log(
-    "¡Victoria para el estudiante valiente que, con coraje y determinación, ha destruido todos los Horrocruxes!"
-  );
-  console.log(
-    "Con cada fragmento de alma oscura eliminado, la luz de la esperanza ha brillado más brillante sobre el mundo mágico."
-  );
-  console.log(
-    "¡Su sacrificio y valentía han salvado a nuestra comunidad de las sombras de la oscuridad, asegurando un futuro lleno de paz y prosperidad para todas las generaciones venideras!"
-  );
-}
-
-function FINAL_MALO() {
-  console.log("En el oscuro manto de la derrota...");
-  console.log(
-    "El estudiante enfrentó una verdad devastadora: a pesar de sus esfuerzos incansables, los Horrocruxes permanecen intactos, y la sombra del mal se alza triunfante sobre el mundo mágico."
-  );
-  console.log(
-    "Aunque la batalla fue ardua y valiente, el destino ha dictado su veredicto, dejando al estudiante con el amargo sabor de la derrota."
-  );
-  console.log(
-    "Pero incluso en la oscuridad más profunda, la llama de la esperanza aún arde, recordando que la lucha nunca termina y que el mañana siempre guarda la promesa de una nueva oportunidad para la redención y la victoria."
-  );
-}
 
